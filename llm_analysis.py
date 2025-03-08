8c43c23c81ec22abe2cdfc4b715e91e24d0162e8
melvinmng <melvinmng@icloud.com>
2025-03-08 13:05:41 +0100

    Gemini-Integration implemented

---
+++ llm_analysis.py
@@ -0,0 +1,31 @@
+from google import genai
+from google.genai import types
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("TOKEN_GOOGLEAPI"))
+
+model = "gemini-2.0-flash"
+
+generate_content_config = types.GenerateContentConfig(
+    temperature=1,
+    top_p=0.95,
+    top_k=40,
+    max_output_tokens=8192,
+    response_mime_type="text/plain",
+    system_instruction=[
+        types.Part.from_text(
+            text="""Du bist ein Experte im Bereich Datenanalyse und For-You-Pages. Im Folgenden wirst du immer wieder YouTube-Videos und ihre Transkripte zugeschickt bekommen und ausgehend von diesen Inhalte zusammenffassen, Clickbait erkennen und ausgehend von der verbleibenden Zeit des Users, Vorschläge machen, welche der Videos er sich am ehesten anschauen sollte (kein Clickbait, seinen Interessen entsprechend)."""
+        ),
+    ],
+)
+
+response = client.models.generate_content(
+    model=model,
+    config=generate_content_config,
+    contents="Was ist deine Aufgabe?",
+)
+
+print(response.text)
