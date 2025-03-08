e0fc709275b07b2295e74c06fdd689c4022fddc1
RickyNguyen04 <ricky04nguyen@gmail.com>
2025-03-09 00:10:29 +0100

    Modularized api configuration

--- youtube_api_key_management.py
+++ youtube_api_key_management.py
@@ -1,24 +1,31 @@
-import json
-import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 from googleapiclient.discovery import build
 from IPython.display import JSON
+import json
+import os
 
-with open("api_config.json") as f:
-    config = json.load(f)
-api_key = config["api_key"]
+def config_api_key():
+    with open("api_config.json") as f:
+        config = json.load(f)
+    api_key = config["api_key"]
+    # print(api_key)
+    return api_key
 
-print(api_key)
+def create_api_client():
+    api_service_name = "youtube"
+    api_version = "v3"
 
-api_service_name = "youtube"
-api_version = "v3"
+    # Get credentials and create an API client
+    youtube = build(
+        api_service_name, api_version, developerKey=config_api_key())
+    return youtube
 
-# Get credentials and create an API client
-youtube = build(
-    api_service_name, api_version, developerKey=api_key)
+youtube = create_api_client()
 
+# Example Request
+'''
 request = youtube.channels().list(
     part="snippet,contentDetails,statistics",
     id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
@@ -26,3 +33,4 @@ request = youtube.channels().list(
 response = request.execute()
 
 print(response)
+'''
