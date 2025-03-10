912f9b4fea20b5c6def2dd8cf52ae885b7f8e45f
melvinmng <melvinmng@icloud.com>
2025-03-10 19:58:41 +0100

    improved imports in test_key_management

--- tests/test_key_management.py
+++ tests/test_key_management.py
@@ -1,18 +1,17 @@
 import pytest
-import src.key_management
-import src.key_management.gemini_api_key_management
-import src.key_management.youtube_api_key_management
+from src.key_management.gemini_api_key_management import get_api_key
+from src.key_management.youtube_api_key_management import load_api_key
 
 
 def test_get_gemini_api_key():
     try:
-        src.key_management.gemini_api_key_management.get_api_key()
+        get_api_key()
     except Exception as e:
         pytest.fail(f"Test ist fehlgeschlagen, weil ein Fehler auftrat: {e}")
 
 
 def test_load_youtube_api_key():
     try:
-        src.key_management.youtube_api_key_management.load_api_key()
+        load_api_key()
     except Exception as e:
         pytest.fail(f"Test ist fehlgeschlagen, weil ein Fehler auftrat: {e}")
