33aba3e831326b37374b06ab585d5f0b4cb897aa
melvinmng <melvinmng@icloud.com>
2025-03-10 18:12:32 +0100

    test_key_management fertig

--- tests/test_key_management.py
+++ tests/test_key_management.py
@@ -1,5 +1,19 @@
+import pytest
 import src.key_management
+import src.key_management.gemini_api_key_management
+import src.key_management.youtube_api_key_management
 
 
 def test_get_gemini_api_key():
-    pass
+    try:
+        src.key_management.gemini_api_key_management.get_api_key()
+    except Exception as e:
+        pytest.fail(f"Test ist fehlgeschlagen, weil ein Fehler auftrat: {e}")
+
+
+@pytest.mark.xfail(reason="YouTube-API-Key noch nicht in .env-Datei")
+def test_load_youtube_api_key():
+    try:
+        src.key_management.youtube_api_key_management.load_api_key()
+    except Exception as e:
+        pytest.fail(f"Test ist fehlgeschlagen, weil ein Fehler auftrat: {e}")
