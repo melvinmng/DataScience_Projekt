4e8f929010ab58280e0c4d999d851a1d1b961229
melvinmng <melvinmng@icloud.com>
2025-04-14 21:59:20 +0200

    Added Note to test_config_env.py

--- tests/env_management/test_config_env.py
+++ tests/env_management/test_config_env.py
@@ -5,6 +5,11 @@ import os
 
 # === Deprecated ===
 
+# NOTE: In order to prevent FileNotFoundErrors during tests, the global patch prevent_load_dotenv_globally has been implemented.
+#       As this patch conflicts with the former test_load_dotenv_called, this test has to be skipped now and
+#       can only be tested when the global patch is deactivated.
+#       To learn more, read the Docstring of prevent_load_dotenv_globally in tests/conftest.py
+
 
 @pytest.mark.skip(
     reason="Not used anymore as it conflicts with the global patch 'prevent_load_dotenv_globally' used to prevent FileNotFoundErrors"
